ef8bba103d13f09f4fb6d19b2be555cba8fc9067
hetao <hetao01@163.com>
2021-06-04 15:06:02 +0800

    Initial commit

---
+++ app/src/main/java/tt/reducto/MainActivity.kt
@@ -0,0 +1,12 @@
+package tt.reducto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tt.reducto.log.TTLog
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
